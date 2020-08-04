97822cce227976415a842d6f5d462877c26c1b7b
Mateen <makjavaprogrammer@gmail.com>
2020-08-05 01:02:04 +0530

    first

---
+++ src/main/java/com/example/restservice/service/s2.java
@@ -0,0 +1,14 @@
+//package com.example.restservice.service;
+//
+//import com.example.restservice.GreetingController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Service;
+//
+//
+//public abstract class s2 implements GreetingController {
+//
+//    @Override
+//    public Object s2() {
+//        return "s2";
+//    }
+//}
